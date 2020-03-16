796abaccc5c05c1c902a144f1df5090f81e6a7c6
jongmin park <showppark12@naver.com>
2020-03-16 14:33:54 +0900

    SingleLinkedList Practice with professor Kwon

---
+++ SingleLinkedList/Node.java
@@ -0,0 +1,14 @@
+//권오흠교수님 강의를 들으면서 만든 노드
+package jongminlist;
+
+public class Node<T> {
+	private T data;
+	public Node<T> next;
+	
+	public Node(T item) {
+		data = item;
+		next = null;
+	}
+	
+
+}
